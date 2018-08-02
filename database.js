244eee0ed3aa4d24a76940144c4e9975a3c2203e
Kevin Seysen <kevinseysen@MacBook-Pro-de-Kevin.local>
2018-08-02 16:20:14 +0200

    post image

--- database.js
+++ database.js
@@ -184,7 +184,7 @@ myRouter.route('/setups/:setups_id')
 
 // Upload a new image with description
 myRouter.route('/images')
-.post(upload.single('image'),function(req,res) {
+.post(function(req,res) {
     console.log('creation image');
     var newImage = new Image();
     newImage.filename = req.file.filename;
